186925f64df5d409aa494d1a11a157b321e01019
godzzo <egodzzo@gmail.com>
2018-11-10 09:30:03 +0100

    add generator to teleshop

--- incjoe.sh
+++ incjoe.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ls -1  | grep tp_ | while read prename; do name=`echo -en $prename | sed 's/tp_//gi'`; echo $name; node ../do_include_html.js ./tp_$name ./$name; done
+ls -1  | grep tp_ | while read prename; do name=`echo -en $prename | sed 's/tp_//gi'`; echo $name; node ../../do_include_html.js ./tp_$name ./$name; done
 
